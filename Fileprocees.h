bf0d0cb574d221a48424eb3a1e59fca3e988c422
billow-css <1564791960@qq.com>
2025-12-09 19:24:52 +0800

    Add AgendaItem.h include and fix exportCard type

--- Fileprocees.h
+++ Fileprocees.h
@@ -2,16 +2,19 @@
 #define FILEPROCESS_H
 
 #include "Agenda.h"
+#include "AgendaItem.h"
 
 class FileProcess {
 public:
     static bool importList(Agenda &agenda, string file_path);
     static bool exportList(const Agenda &agenda, string file_path);
     static bool importCard(Agenda &agenda, string file_path);
-    static bool exportCard(const Agendaltern &item, string file_path);
+    static bool exportCard(const AgendaItem &item, string file_path);
     static bool validateFilePath(string file_path);
     static string getFileExtension(string file_path);
     static bool isFileExist(string file_path);
 };
 
+
+#endif // FILEPROCESS_H
 
